45310f7d5223b2db15001d176cc6cb783eb67453
Ulrich Eck <ueck@net-labs.de>
2015-01-27 01:18:42 +1030

    initial tests successful with BasicFacade :)

--- src/Ubitrack_libs.jl
+++ src/Ubitrack_libs.jl
@@ -1,7 +1,1 @@
-ubitrack_libraries = ["libutcore.1.0.0",
-                      "libutdataflow.1.0.0",
-                      "libutfacade.1.0.0",
-                      "libutvision.1.0.0"]
-
-# missing:
-# uthaptics for now ..
+ubitrack_libraries = ["libutfacade.1.0.0"]
